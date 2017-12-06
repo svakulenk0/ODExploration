433a0c4206a74718d9fa435ee6ea201fd9f3c21d
vendi <svitlana.vakulenko@gmail.com>
2017-12-06 16:56:15 +0100

    chg: keywords for aggregation

--- load_ES.py
+++ load_ES.py
@@ -32,7 +32,8 @@ class ESClient():
         '''
         returns the most popular keywords
         '''
-        result = self.es.search(index=self.index, body={"query": {"match_all": {}}, "aggs": {"licenses": {"terms": {"field": "raw.license_id.keyword"}}, "categories": {"terms": {"field": "raw.categorization.keyword"}}}})
+        # result = self.es.search(index=self.index, body={"query": {"match_all": {}}, "aggs": {"licenses": {"terms": {"field": "raw.license_id.keyword"}}, "categories": {"terms": {"field": "raw.categorization.keyword"}}}})
+        result = self.es.search(index=self.index, body={"query": {"match_all": {}}, "aggs": {"tags": {"terms": {"field": "raw.tags.name.keyword"}}, "organizations": {"terms": {"field": "raw.organization.name.keyword"}}}})
         return result['aggregations']
 
 
