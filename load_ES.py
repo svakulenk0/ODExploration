3a6e7c78b99ca6c60f5fb35f4e83f47310a0eea1
vendi <svitlana.vakulenko@gmail.com>
2017-12-14 15:36:28 +0100

    new: add title as an attribute

--- load_ES.py
+++ load_ES.py
@@ -32,19 +32,19 @@ class ESClient():
 
     def top(self):
         '''
-        returns the most popular keywords
+        returns the most popular entities
         '''
         result = self.es.search(index=self.index, body={"query": {"match_all": {}}, "aggs": {
-                "licenses": {"terms": {"field": "raw.license_id.keyword"}},
-                "categories": {"terms": {"field": "raw.categorization.keyword"}},
+                "title": {"terms": {"field": "raw.title.keyword"}},
+                "license": {"terms": {"field": "raw.license_id.keyword"}},
+                "organization": {"terms": {"field": "raw.categorization.keyword"}},
                 "tags": {"terms": {"field": "raw.tags.name.keyword"}},
-                "organizations": {"terms": {"field": "raw.organization.name.keyword"}}
+                "organization": {"terms": {"field": "raw.organization.name.keyword"}}
             }})
         return result['aggregations']
 
     def count(self):
         '''
-        returns the most popular keywords
         '''
         result = self.es.search(index=self.index, body={"query": {"match_all": {}}, "aggs": {
                 "licenses": {"cardinality": {"field": "raw.license_id.keyword"}},
@@ -63,8 +63,8 @@ def test_index(index=INDEX):
 
 def test_aggregation_stats(index=INDEX):
     db = ESClient(index)
-    # print db.top()
-    print db.count()
+    print db.top()
+    # print db.count()
 
 
 if __name__ == '__main__':
