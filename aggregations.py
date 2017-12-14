dc39449e94607221d5f724c387367a04f7691bb4
vendi <svitlana.vakulenko@gmail.com>
2017-12-14 16:26:55 +0100

    chg: updated counts

--- aggregations.py
+++ aggregations.py
@@ -14,29 +14,7 @@ counts = {
     }
 
 top_keywords = {
-      u'licenses': {
-        u'buckets': [
-          {
-            u'key': u'cc-by-at-30',
-            u'doc_count': 1852
-          },
-          {
-            u'key': u'cc-by',
-            u'doc_count': 97
-          },
-          {
-            u'key': u'other-pd',
-            u'doc_count': 51
-          },
-          {
-            u'key': u'cc-by-sa',
-            u'doc_count': 2
-          }
-        ],
-        u'sum_other_doc_count': 0,
-        u'doc_count_error_upper_bound': 0
-      },
-      u'organizations': {
+      u'organization': {
         u'buckets': [
           {
             u'key': u'stadt-linz',
@@ -81,52 +59,6 @@ top_keywords = {
         ],
         u'sum_other_doc_count': 332,
         u'doc_count_error_upper_bound': 0
-      },
-      u'categories': {
-        u'buckets': [
-          {
-            u'key': u'finanzen-und-rechnungswesen',
-            u'doc_count': 198
-          },
-          {
-            u'key': u'umwelt',
-            u'doc_count': 186
-          },
-          {
-            u'key': u'{bevoelkerung}',
-            u'doc_count': 154
-          },
-          {
-            u'key': u'{geographie-und-planung}',
-            u'doc_count': 145
-          },
-          {
-            u'key': u'{verkehr-und-technik}',
-            u'doc_count': 111
-          },
-          {
-            u'key': u'{verwaltung-und-politik}',
-            u'doc_count': 94
-          },
-          {
-            u'key': u'["umwelt"]',
-            u'doc_count': 83
-          },
-          {
-            u'key': u'["verwaltung-und-politik"]',
-            u'doc_count': 72
-          },
-          {
-            u'key': u'{wirtschaft-und-tourismus}',
-            u'doc_count': 56
-          },
-          {
-            u'key': u'["geographie-und-planung"]',
-            u'doc_count': 52
-          }
-        ],
-        u'sum_other_doc_count': 851,
-        u'doc_count_error_upper_bound': 20
       },
       u'tags': {
         u'buckets': [
@@ -173,5 +105,119 @@ top_keywords = {
         ],
         u'sum_other_doc_count': 9795,
         u'doc_count_error_upper_bound': 63
+      },
+      u'categorization': {
+        u'buckets': [
+          {
+            u'key': u'finanzen-und-rechnungswesen',
+            u'doc_count': 198
+          },
+          {
+            u'key': u'{umwelt}',
+            u'doc_count': 186
+          },
+          {
+            u'key': u'{bevoelkerung}',
+            u'doc_count': 154
+          },
+          {
+            u'key': u'{geographie-und-planung}',
+            u'doc_count': 145
+          },
+          {
+            u'key': u'{verkehr-und-technik}',
+            u'doc_count': 111
+          },
+          {
+            u'key': u'{verwaltung-und-politik}',
+            u'doc_count': 94
+          },
+          {
+            u'key': u'["umwelt"]',
+            u'doc_count': 83
+          },
+          {
+            u'key': u'["verwaltung-und-politik"]',
+            u'doc_count': 72
+          },
+          {
+            u'key': u'{wirtschaft-und-tourismus}',
+            u'doc_count': 56
+          },
+          {
+            u'key': u'["geographie-und-planung"]',
+            u'doc_count': 52
+          }
+        ],
+        u'sum_other_doc_count': 851,
+        u'doc_count_error_upper_bound': 20
+      },
+      u'license': {
+        u'buckets': [
+          {
+            u'key': u'cc-by-at-30',
+            u'doc_count': 1852
+          },
+          {
+            u'key': u'cc-by',
+            u'doc_count': 97
+          },
+          {
+            u'key': u'other-pd',
+            u'doc_count': 51
+          },
+          {
+            u'key': u'cc-by-sa',
+            u'doc_count': 2
+          }
+        ],
+        u'sum_other_doc_count': 0,
+        u'doc_count_error_upper_bound': 0
+      },
+      u'title': {
+        u'buckets': [
+          {
+            u'key': u'Denkmalliste Steiermark',
+            u'doc_count': 2
+          },
+          {
+            u'key': u'Denkmalliste Wien',
+            u'doc_count': 2
+          },
+          {
+            u'key': u'Ehescheidungen',
+            u'doc_count': 2
+          },
+          {
+            u'key': u'Polytechnische Schule',
+            u'doc_count': 2
+          },
+          {
+            u'key': u'Sonderschulen',
+            u'doc_count': 2
+          },
+          {
+            u'key': u'Voranschlag 2015',
+            u'doc_count': 2
+          },
+          {
+            u'key': u'1 und 2 Stern Beherbergungsbetriebe',
+            u'doc_count': 1
+          },
+          {
+            u'key': u'3 Stern Beherbergungsbetriebe',
+            u'doc_count': 1
+          },
+          {
+            u'key': u'3D Geodaten mit Level of Detail2',
+            u'doc_count': 1
+          },
+          {
+            u'key': u'5 und 4 Stern Beherbergungsbetriebe',
+            u'doc_count': 1
+          }
+        ],
+        u'sum_other_doc_count': 2012,
+        u'doc_count_error_upper_bound': 5
       }
     }
