4c73cf1499ea3d20d92104bf9525579eb607a241
vendi <svitlana.vakulenko@gmail.com>
2017-12-18 13:08:03 +0100

    chg: return more dataset titles

--- load_ES.py
+++ load_ES.py
@@ -35,7 +35,7 @@ class ESClient():
         get stats for the search subsample of the information space
         '''
         result = self.es.search(index=self.index, explain=True, body={"query": {"match": {"_all": keywords}}, "aggs": {
-                "title": {"terms": {"field": "raw.title.keyword", "size" : n}},
+                "title": {"terms": {"field": "raw.title.keyword", "size" : 400}},
                 "license": {"terms": {"field": "raw.license_id.keyword", "size" : n}},
                 "categorization": {"terms": {"field": "raw.categorization.keyword", "size" : n}},
                 "tags": {"terms": {"field": "raw.tags.name.keyword", "size" : n}},
