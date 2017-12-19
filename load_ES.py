4a1a8c6fe3b991f2578842815053850c93c6a41a
vendi <svitlana.vakulenko@gmail.com>
2017-12-19 11:23:59 +0100

    chg: debug search for a geo query

--- load_ES.py
+++ load_ES.py
@@ -109,7 +109,8 @@ def test_aggregation_stats(index=INDEX):
 def test_describe_subset(index=INDEX, top_n=2):
     db = ESClient(index)
     keyword = "I would like to know more about finanzen"
-    results = db.describe_subset(keyword, top_n=2)
+    keyword = "stadt wien"
+    results = db.describe_subset(keyword, top_n=top_n)
     print json.dumps(results, indent=4, sort_keys=True)
     # pick the most representative documents from the subset
 
