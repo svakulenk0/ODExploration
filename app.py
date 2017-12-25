d149a40a3ab7160194fef4770fb88ac4008ee2b8
vendi <svitlana.vakulenko@gmail.com>
2017-12-25 13:22:14 +0100

    new: web UI

---
+++ app.py
@@ -0,0 +1,30 @@
+'''
+svakulenko
+22 Dec 2017
+
+Flas web app for chatbot interface based on https://github.com/chamkank/flask-chatterbot
+'''
+
+from flask import Flask, render_template, request
+
+from dialog_agent import DialogAgent
+
+app = Flask(__name__)
+
+chatbot = DialogAgent()
+# chatbot.chat(greeting="Welcome, I will show you around data.gv.at", simulate=False)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(chatbot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
