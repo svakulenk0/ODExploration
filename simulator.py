1d8a22918112588481f19c1845b00ba2db1f53f7
vendi <svitlana.vakulenko@gmail.com>
2018-01-03 16:39:55 +0100

    new: seeker simulator class

--- simulator.py
+++ simulator.py
@@ -2,20 +2,60 @@
 svakulenko
 3 Jan 2017
 
-User simulator to evaluate dialog agent interaction performance.
+User simulator class to evaluate dialog agent interaction performance.
 '''
 from dialog_agent import DialogAgent
 from load_ES import ESClient, INDEX_LOCAL
 
 
-def test_BFS(index=INDEX_LOCAL, n_turns=15):
+class Seeker():
+    '''
+    '''
+    def __init__(self, time, strategy):
+        # number of dialogue turns, time T
+        self.time = time
+        if strategy == 'bfs':
+            self.reply = self.bfs
+        elif strategy == 'dfs':
+            self.reply = self.dfs
+
+    def bfs(self):
+        '''
+        ask for diversity in results, horizontal search
+        '''
+        return "other"
+
+    def dfs(self):
+        '''
+        vertical search, get exhaustive list of results
+        '''
+        return "more"
+
+
+def test_BFS(index=INDEX_LOCAL):
     '''
     Default search strategy is breadth-first exploring all important facets
     '''
     chatbot = DialogAgent(index, spacing='\n')
-    for i in range(n_turns):
-        user_message = "ok"
+    user = Seeker(time=5, strategy='bfs')
+    for i in range(user.time):
         # user says
+        user_message = user.reply()
+        print user_message
+        # bot says
+        print chatbot.get_response(user_message)
+        print '\n'
+
+
+def test_DFS(index=INDEX_LOCAL, n_turns=15):
+    '''
+    Default search strategy is breadth-first exploring all important facets
+    '''
+    chatbot = DialogAgent(index, spacing='\n')
+    user = Seeker(time=5, strategy='dfs')
+    for i in range(user.time):
+        # user says
+        user_message = user.reply()
         print user_message
         # bot says
         print chatbot.get_response(user_message)
