2d311a9858449f6314b275d1c6b319de1df7fda9
vendi <svitlana.vakulenko@gmail.com>
2018-01-22 11:31:25 +0100

    new: loop through the parameter space

--- simulator.py
+++ simulator.py
@@ -17,6 +17,8 @@ class Seeker():
     def __init__(self):
         # establish connection to the database
         self.db = ESClient()
+        # model user knowledge
+        self.memory = set()
         # cognitive resource limit
         self.l = 9
         # initialize goal to a random concept from the information model
@@ -28,20 +30,33 @@ class Seeker():
     def define_goal(self):
         # pick at item from the database at random
         # entities_list = [entity['key'] for facet in entities.values() for entity in facet['buckets']]
-        self.goal = self.db.get_random_item()
+        doc = self.db.get_random_doc()
+
+        self.goal = set(self.db.compile_item_entities(doc))
         # self.goal = random.choice(items)
 
     def reply_random(self, actions):
         # check set intersect with the goal
-        print self.goal
-        print actions
-        # concepts = [concept for action.val]
-        if not set(self.goal) & set(actions):
+        # print self.goal
+        # print actions
+        action_set = set(actions)
+        # model perception mechanism
+        self.memory = self.memory.union(action_set)
+        # print self.memory
+
+        # check success criteria, i.e. if we reached the goal
+        # print "The user is looking for:", self.goal.difference(self.memory)
+        if self.goal.issubset(self.memory):
+            return 'Thank you!'
+
+        match = action_set.intersection(self.goal)
+        if not action_set.intersection(self.goal):
             # the goal is not reached yet, continue exploration
-            action = random.choice(actions)
-            return action
+            # action = random.choice(actions)
+            return 'Continue'
         else:
-            return 'Yes'
+            return list(match)[0]
+
 
 
 def test_define_random_goal():
@@ -52,33 +67,65 @@ def test_define_random_goal():
     print user.goal
 
 
-def simulate(n=12):
+def simulate(l):
     '''
     Simulate conversation between a user and a dialog agent
-    n <int> number of turns
     '''
     # initialize conversation partners
     user = Seeker()
-    chatbot = DialogAgent()
-    action = None
+    chatbot = DialogAgent(l)
     # start the conversation
     # show default greeting
     print 'A:', chatbot.greeting
     # start exploration
-    message, actions = chatbot.chat(action)
-    print 'A:', message
-    for i in range(n):
+    message, actions = chatbot.chat()
+    print message
+
+    # count number of turns
+    n_turns = 1
+    while True:
         # choose exploration direction
         if actions:
             action = user.chat(actions)
-            print 'U:', action
+            if isinstance(action, tuple):
+                message = action[1]
+            else:
+                message = action
+            print '\nU:%s\n' % message
             # success-stopping condition
-            if action == 'Yes':
+            if action == 'Thank you!':
                 break
         # continue exploration
         message, actions = chatbot.chat(action)
         print 'A:', message
+        n_turns += 1
+
+    return n_turns
+
+
+def run_evaluation(l=8, k=500):
+    '''
+    Run simulation n times:
+
+    k <int> number of runs
+    '''
+    n_turns = []
+    for i in range(k):
+        n_turns.append(simulate(l))
+
+    # show stats
+    print "Cognitive resource:", l
+    print "\nDistribution of the number of turns per dialog (%d runs):" % k
+    print sorted(n_turns)
+    print "Minimum number of turns per dialog", min(n_turns)
+    print "Average number of turns per dialog", reduce(lambda x, y: x + y, n_turns) / len(n_turns)
+    print "Maximum number of turns per dialog", max(n_turns)
+
+
+def evaluate_sensitivity():
+    for l in [3, 4, 5, 6, 7, 8]:
+        run_evaluation(l)
 
 
 if __name__ == '__main__':
-    simulate()
+    evaluate_sensitivity()
