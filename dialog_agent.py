22575b6b9eb29c9ed2ba7097b205374c1a0c3148
vendi <svitlana.vakulenko@gmail.com>
2018-01-23 14:43:32 +0100

    new: show titles up to the limit

--- dialog_agent.py
+++ dialog_agent.py
@@ -23,7 +23,7 @@ class DialogAgent():
         self.rank = []
         # default greeting message
         self.greeting = "Hi! Welcome to the Austrian Open Data portal!"
-        self.entity_decorator = "<button class='item' onclick=pivotEntity({%s})>%s</button>"
+        self.entity_decorator = '''<button class='item' onclick="pivotEntity('%s','%s')">%s</button>'''
         # maximum message size
         self.l = l
         # default exploration direction corresponds to the whole information space
@@ -42,7 +42,7 @@ class DialogAgent():
         n = result['hits']['total']
 
         # form message
-        if 'aggregations' in result.keys() and n > 1:
+        if 'aggregations' in result.keys() and n > self.l:
             entities = result['aggregations']
             # create chunks
             chunks = chunk_w_ranks(entities)
@@ -50,24 +50,31 @@ class DialogAgent():
             chunks_rank = rank_chunks(chunks, self.l, self.history)
             facet, entities = chunks_rank.get()[1]
             if self.simulation:
-                message = "There are %d datasets.\nYou can explore them by %s:\n\n%s" % (n, facet, '\n'.join(entities))
+                message = "\nThere are %d datasets.\nYou can explore them by %s, e.g.:\n\n%s" % (n, facet, '\n'.join(entities))
             else:
                 # web-based chat html
-                buttons = '<br>'.join(self.entity_decorator % ((facet, entity), entity) for entity in entities)
-                message = "There are %d datasets.<br>You can explore them by %s:<br><br>%s" % (n, facet, buttons)
+                buttons = '<br>'.join(self.entity_decorator % (facet, entity, entity) for entity in entities)
+                message = "<br>There are %d datasets.<br>You can explore them by %s, e.g.:<br><br>%s" % (n, facet, buttons)
             concepts = [(facet, entity) for entity in entities]
             self.history.extend(concepts)
             return message.encode('utf8'), concepts
         # found it
         elif n > 0:
-            message = "Here you are:"
+            message = "<br>Here you are:"
             concepts = []
             for i, doc in enumerate(result['hits']['hits']):
                 # show all entities of the item
                 concepts.extend(self.db.compile_item_entities(doc['_source']))
-                message += "\n%s. Dataset\n\n%s" % (i+1, '\n'.join(["%s: %s" % (facet, entity) for facet, entity in concepts]))
+                if self.simulation:
+                    message += "\n\n%s" % (i+1, '\n'.join(["%s: %s" % (facet, entity) for facet, entity in concepts]))
+                else:
+                # web-based chat html
+                    message += "<br><br>%s" % (i+1, '<br>'.join(["%s: %s" % (facet, entity) for facet, entity in concepts]))
             return message.encode('utf8'), concepts
         else:
+            # reset goal to the whole information space
+            self.goal = []
+            self.history = []
             return "No matching datasets found", []
 
 
