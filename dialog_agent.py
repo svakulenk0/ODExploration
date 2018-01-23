32151d94927fe16885878fd6f924a506aabc5f32
vendi <svitlana.vakulenko@gmail.com>
2018-01-23 14:46:28 +0100

    fix

--- dialog_agent.py
+++ dialog_agent.py
@@ -66,10 +66,10 @@ class DialogAgent():
                 # show all entities of the item
                 concepts.extend(self.db.compile_item_entities(doc['_source']))
                 if self.simulation:
-                    message += "\n\n%s" % (i+1, '\n'.join(["%s: %s" % (facet, entity) for facet, entity in concepts]))
+                    message += "\n\n" + '\n'.join(["%s: %s" % (facet, entity) for facet, entity in concepts])
                 else:
                 # web-based chat html
-                    message += "<br><br>%s" % (i+1, '<br>'.join(["%s: %s" % (facet, entity) for facet, entity in concepts]))
+                    message += "<br><br>" + '<br>'.join(["%s: %s" % (facet, entity) for facet, entity in concepts])
             return message.encode('utf8'), concepts
         else:
             # reset goal to the whole information space
