32aed95f3d603d0b4005ac509632d1d5e7572b0c
vendi <svitlana.vakulenko@gmail.com>
2018-01-23 13:39:29 +0100

    fix: remove unused web chat calls

--- app.py
+++ app.py
@@ -18,52 +18,53 @@ chatbot = DialogAgent()
 def home():
     # start exploration
     message, actions = chatbot.chat()
-    message = message.replace('\n', '<br>')
+    # message = message.replace('\n', '<br>')
     return render_template("index.html", text=message)
 
 
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    message, concepts = chatbot.chat(userText)
-    return str(message.encode('utf8'))
+# @app.route("/get")
+# def get_bot_response():
+#     userText = request.args.get('msg')
+#     message, concepts = chatbot.chat(userText)
+#     return str(message.encode('utf8'))
 
 
-@app.route("/show")
-def show_dataset():
-    dataset_id = request.args.get('msg')
-    return str(chatbot.show_dataset(dataset_id).encode('utf8'))
+# @app.route("/show")
+# def show_dataset():
+#     dataset_id = request.args.get('msg')
+#     return str(chatbot.show_dataset(dataset_id).encode('utf8'))
 
 
-@app.route("/facets")
-def show_facets():
-    return str(chatbot.show_facets().encode('utf8'))
+# @app.route("/facets")
+# def show_facets():
+#     return str(chatbot.show_facets().encode('utf8'))
 
 
-@app.route("/samples")
-def show_samples():
-    facet = request.args.get('facet')
-    entity = request.args.get('entity')
-    return str(chatbot.subset(facet, entity).encode('utf8'))
+# @app.route("/samples")
+# def show_samples():
+#     facet = request.args.get('facet')
+#     entity = request.args.get('entity')
+#     return str(chatbot.subset(facet, entity).encode('utf8'))
 
 @app.route("/pivot")
 def pivot_entity():
-    facets_entities = json.loads(request.args.get('facets_entities'))
-    return str(chatbot.pivot(facets_entities).encode('utf8'))
+    action = json.loads(request.args.get('facets_entities'))
+    message, actions = chatbot.chat(action)
+    return message
 
 
-@app.route("/summary")
-def summarize_items():
-    return str(chatbot.summarize_items().encode('utf8'))
+# @app.route("/summary")
+# def summarize_items():
+#     return str(chatbot.summarize_items().encode('utf8'))
 
 
-@app.route("/more")
-def get_more():
-    samples = chatbot.sample_nodes(size=10)
-    if samples:
-        return str(samples.encode('utf8'))
-    else:
-        return str(chatbot.get_response("").encode('utf8'))
+# @app.route("/more")
+# def get_more():
+#     samples = chatbot.sample_nodes(size=10)
+#     if samples:
+#         return str(samples.encode('utf8'))
+#     else:
+#         return str(chatbot.get_response("").encode('utf8'))
 
 
 if __name__ == "__main__":
