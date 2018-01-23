83d448a8f68b281bffed25e334fa82677b62194e
vendi <svitlana.vakulenko@gmail.com>
2018-01-23 10:59:26 +0100

    new: max number of entities parameter

--- dialog_agent.py
+++ dialog_agent.py
@@ -14,7 +14,7 @@ class DialogAgent():
     Dialog agent for the conversational browsing task
     '''
 
-    def __init__(self):
+    def __init__(self, l=8):
         # establish connection to the database
         self.db = ESClient()
         # concepts already communicated to the user
@@ -24,36 +24,43 @@ class DialogAgent():
         # default greeting message
         self.greeting = "Hi! Welcome to the Austrian Open Data portal!"
         # maximum message size
-        self.l = 4
+        self.l = l
+        # default exploration direction corresponds to the whole information space
+        self.goal = []
 
-    def chat(self, action=[]):
-        # get subset of the information model, {} corresponds to the whole information space
-        if action:
-            entities, n = self.db.summarize_subset(facets_values=[action])
-        else:
-            entities, n = self.db.summarize_subset()
+    def chat(self, action='Continue'):
+        # print action
+        if action != 'Continue':
+            # default exploration direction corresponds to the whole information space
+            self.goal.append(action)
+        # print 'Focus:', self.goal
+        # get subset of the information space
+        result = self.db.summarize_subset(facets_values=self.goal)
+        n = result['hits']['total']
+
         # form message
-        if n > 0:
+        if 'aggregations' in result.keys() and n > 1:
+            entities = result['aggregations']
             # create chunks
             chunks = chunk_w_ranks(entities)
             # rank chunks
             chunks_rank = rank_chunks(chunks, self.l, self.history)
-            facet, concepts = chunks_rank.get()[1]
-            message = "There are %d datasets with %s: %s" % (n, facet, ', '.join(concepts))
-            # show message
-            # print 'A:', message
+            facet, entities = chunks_rank.get()[1]
+            message = "There are %d datasets.\nYou can explore them by %s:\n\n%s" % (n, facet, '\n'.join(entities))
+            concepts = [(facet, entity) for entity in entities]
             self.history.extend(concepts)
-            actions = [(facet, concept) for concept in concepts]
-            return message, actions
+            return message, concepts
+        # found it
+        elif n > 0:
+            message = "Here you are:"
+            concepts = []
+            for i, doc in enumerate(result['hits']['hits']):
+                # show all entities of the item
+                concepts.extend(self.db.compile_item_entities(doc['_source']))
+                message += "\n%s. Dataset\n\n%s" % (i+1, '\n'.join(["%s: %s" % (facet, entity) for facet, entity in concepts]))
+            return message, concepts
         else:
-            return "No matching datasets found", {}
-
-            # 4. users turn
-            # if simulate:
-            #     user_message = "Hi!"
-            #     print 'U:', user_message
-            # else:
-            #     user_message = raw_input()
+            return "No matching datasets found", []
 
 
 def test_DialogAgent():
