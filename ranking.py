a378a7e3eb5d751f54e7a6905e779328a0171fba
vendi <svitlana.vakulenko@gmail.com>
2018-01-23 11:45:53 +0100

    fix: check history

--- ranking.py
+++ ranking.py
@@ -6,6 +6,7 @@ Ranking for nodes and chunks of the information model
 '''
 from Queue import PriorityQueue
 from heapq import heappush, nlargest
+import numpy as np
 
 from aggregations import entities, n_items
 
@@ -52,12 +53,6 @@ def chunk(entities):
 
 def chunk_w_ranks(entities):
     chunks = {}
-
-    # facets chunk
-    # chunks['facets'] = []
-    # for facet, count in facets.items():
-    #     chunks['facets'].append((n_items/count['value'], facet))
-
     # entities chunk per facet
     for facet, counts in entities.items():
         chunks[facet] = []
@@ -81,13 +76,66 @@ def rank_chunks(chunks, l, history=[]):
         chunk_sum = 0
         concepts = []
         for rank, concept in chunk:
-            if concept not in history:
+            if (facet, concept) not in history:
                 chunk_sum += rank
                 concepts.append(concept)
             if len(concepts) >= l:
                 break
         concept_rank.put((-chunk_sum, (facet, concepts)))
     return concept_rank
+
+
+def gini(x):
+    '''
+    Relative mean absolute difference (rmad) / 2
+    from https://stackoverflow.com/questions/39512260/calculating-gini-coefficient-in-python-numpy
+    '''
+    # (Warning: This is a concise implementation, but it is O(n**2)
+    # in time and memory, where n = len(x).  *Don't* pass in huge
+    # samples!)
+
+    # Mean absolute difference (standard deviation?)
+    mad = np.abs(np.subtract.outer(x, x)).mean()
+    # Relative mean absolute difference (average)
+    mean = np.mean(x)
+    # print mean
+    rmad = mad/mean
+    # Gini coefficient
+    g = 0.5 * rmad
+    return abs(g)
+
+
+def gini_facets(entities):
+    '''
+    analyse skewness of the distribution among the entites within the attribute
+    compute a score for each attribute characterizing the skewness
+    of the information mass distribution
+    to quantify the discriminatory power of the individual factors (attributes)
+    '''
+    #  iterate over attributes
+    # facets_rank = PriorityQueue()
+    # for facet, counts in top_keywords.items():
+    #     # top entities count distribution of the attribute
+    #     distribution = [entity['doc_count'] for entity in counts['buckets']]
+    #     # print distribution
+    #     skewness = gini(distribution)
+    #     facets_rank.put((-skewness, facet))
+    # return facets_rank
+
+    # chunks = {}
+    # entities chunk per facet
+    for facet, counts in entities.items():
+        # chunks[facet] = []
+        # entities count distribution of the attribute
+        distribution = [entity['doc_count'] for entity in counts['buckets']]
+        skewness = gini(distribution)
+        print facet, skewness
+        # chunks[facet].append((entity['doc_count'], entity['key']))
+    # return chunks
+
+
+def test_gini_facets():
+    gini_facets(entities)
 
 
 def test_rank_nodes(topn=20):
@@ -103,7 +151,7 @@ def test_chunk_w_ranks(n=2):
     print chunk_w_ranks(entities)
 
 
-def test_rank_chunks(l=3, n=2):
+def test_rank_chunks(l=3, n=4, entities=entities):
     '''
     l <int> maximum size of the basket
     n <int> number of turns
@@ -112,13 +160,14 @@ def test_rank_chunks(l=3, n=2):
     history = []
     for i in range(n):
         chunks_rank = rank_chunks(chunks, l, history)
-        message = chunks_rank.get()[1]
-        print message
-        history.extend(message.values()[0])
+        facet, entities = chunks_rank.get()[1]
+        print facet, entities
+        concepts = [(facet, entity) for entity in entities]
+        history.extend(concepts)
 
 
 def main():
-    test_rank_chunks()
+    test_gini_facets()
 
 
 if __name__ == '__main__':
