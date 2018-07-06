6e455e6e3deae0f87fe59dbcdc3aca129c140663
vendi <svitlana.vakulenko@gmail.com>
2018-07-06 16:52:00 +0200

    new: search

--- __init__.py
+++ __init__.py
@@ -11,11 +11,20 @@ def setup(opsdroid):
     logging.debug("Loaded ODExploration skill")
 
 
-@match_regex(r'.*')
+@match_regex(r'browse')
 async def exploreOD(opsdroid, config, message):
     chatbot.history = []
     chatbot.goal = []
     # start exploration
     text, actions = chatbot.chat(start=True)
+    print(text)
+    await message.respond(text)
+
+
+@match_regex(r'search .*')
+async def searchOD(opsdroid, config, message):
+    request = message
+    print request
+    text, actions = chatbot.search(request)
     print(text)
     await message.respond(text)
