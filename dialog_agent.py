44e8f11a49e2c46d1e627466c3a8206ba9efbd7a
vendi <svitlana.vakulenko@gmail.com>
2018-07-06 15:10:53 +0200

    fix: python 3

--- dialog_agent.py
+++ dialog_agent.py
@@ -271,8 +271,8 @@ class DialogAgent():
 
         result = self.db.summarize_subset(facets_values=self.goal)
         n = result['hits']['total']
-        print n, 'results'
-        print result
+        print(n, 'results')
+        print(result)
         # form message
         if 'aggregations' in result.keys() and n > self.l:
             message, concepts = self.aggregate_entities(result, message, n)
